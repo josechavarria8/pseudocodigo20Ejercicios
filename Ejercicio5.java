ec7e66a3393609c05cc4a605ab6372f0049afa6e
¨Jose <josechavarrial811@gmail.com>
2021-06-09 21:39:06 -0600

    ejercicio5

---
+++ Ejercicio5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+	public static void main(String args[]) {
+		
+		Scanner scanner = new Scanner(System.in);
+
+		int num;
+		System.out.println("Numero: ");
+		num = scanner.nextInt();
+		System.out.println("El numero siguiente es: "+(num+1));
+	}
+
+
+}
+
